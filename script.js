71db85bbc791a2565136b460f515c24d9b515a95
corsostefano <stefanocorso6@gmail.com>
2025-01-31 11:58:47 -0400

    web

---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var swiper = new Swiper('.logoSwiper', {
+        slidesPerView: 5,
+        spaceBetween: 20,
+        loop: true,
+        centeredSlides: true, // Centra los slides en el carrusel
+        autoplay: {
+            delay: 1000,
+        },
+        breakpoints: {
+            1024: { slidesPerView: 5 },
+            768: { slidesPerView: 3 },
+            480: { slidesPerView: 2 },
+            320: { slidesPerView: 1 }
+        }
+    });
+});
